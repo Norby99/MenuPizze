e5b285ca5074182532cf7f95c21dcd28e8259820
Norby Gabos <dont.vip@gmail.com>
2021-03-24 19:05:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -49,24 +49,24 @@ class Elenco:
                 y += 1
 
     def showAggiunte(self):
-        coords = [25, self.margini[3], (self.margini[2]*(4/5)+500), self.screenDimension[1]-20]
+        coords = [resolutionConverter(25), self.margini[3], (self.margini[2]*(4/5)+resolutionConverter(500)), self.screenDimension[1]-resolutionConverter(20)]
         self.canvas.pack()
 
-        font_titolo = "Times 22 bold"
-        font_aggiunte = "Times 16"
+        font_titolo = "Times " + str(resolutionConverter(22)) + " bold"
+        font_aggiunte = "Times " + str(resolutionConverter(16))
 
         ### TITOLO ###
         self.canvas.create_text(coords[0], coords[1], anchor= tk.NW, fill="black",font=font_titolo, text= "Aggiunte")
 
         ### AGGIUNTE ###
-        self.canvas.create_text(coords[0], coords[3]-5, anchor= tk.SW, fill="black",font=font_aggiunte, text= self.testo_aggiunte, width = (coords[2]-coords[0]))
+        self.canvas.create_text(coords[0], coords[3]-resolutionConverter(5), anchor= tk.SW, fill="black",font=font_aggiunte, text= self.testo_aggiunte, width = (coords[2]-coords[0]))
 
     def scritte(self, coords, pizza):
 
-        font_nome = "Times 20 bold"
-        font_prezzo = "Times 20 bold"
-        font_ingredienti = "Times 15"
-        font_tipo = "Times 36 bold"
+        font_nome = "Times " + str(resolutionConverter(20)) + " bold"
+        font_prezzo = "Times " + str(resolutionConverter(20)) + " bold"
+        font_ingredienti = "Times " + str(resolutionConverter(15))
+        font_tipo = "Times " + str(resolutionConverter(36)) + " bold"
 
         colori = {"Pizze classiche" : "green", "Pizze bianche" : "blue", "Pizze conditissime" : "red"}
 
@@ -76,16 +76,16 @@ class Elenco:
                 pizza["ingredienti"] += " tutto fuori cottura"
             
             ### NOME ###
-            self.canvas.create_text(coords[0]+5, coords[1]+5, anchor= tk.NW, fill="black",font=font_nome, text= pizza["nome"])
+            self.canvas.create_text(coords[0]+resolutionConverter(5), coords[1]+resolutionConverter(5), anchor= tk.NW, fill="black",font=font_nome, text= pizza["nome"])
 
             ### PREZZO ###
-            self.canvas.create_text(coords[2]-10, coords[1]+5, anchor= tk.NE, fill="red",font=font_prezzo, text= pizza["prezzo"])
+            self.canvas.create_text(coords[2]-resolutionConverter(10), coords[1]+resolutionConverter(10), anchor= tk.NE, fill="red",font=font_prezzo, text= pizza["prezzo"])
 
             ### INGREDIENTI ###
-            self.ScritteIngredienti.append(self.canvas.create_text(coords[0]+5, (coords[3]+coords[1])/2-10, anchor= tk.NW, fill="black",font=font_ingredienti, text= pizza["ingredienti"], width=(self.cell_dimension[0]-10)))
+            self.ScritteIngredienti.append(self.canvas.create_text(coords[0]+resolutionConverter(5), (coords[3]+coords[1])/2-resolutionConverter(10), anchor= tk.NW, fill="black",font=font_ingredienti, text= pizza["ingredienti"], width=(self.cell_dimension[0]-resolutionConverter(10))))
         else:
             ### TIPO ###
-            self.canvas.create_text(coords[0]+5, coords[3]-5, anchor= tk.SW, fill=colori[pizza["tipo"]], font=font_tipo, text= pizza["tipo"])
+            self.canvas.create_text(coords[0]+resolutionConverter(5), coords[3]-resolutionConverter(25), anchor= tk.SW, fill=colori[pizza["tipo"]], font=font_tipo, text= pizza["tipo"])
 
     def updateScritte(self):    #this function updates the text boxes
         lingua = self.getLingua()
@@ -114,11 +114,13 @@ class Fullscreen:
         self.window.bind("<F12>", self.close)
         self.window.bind("<F11>", self.toggleFullScreen)
         self.window.bind("<Escape>", self.quitFullScreen)
-        self.screenDimension = 2560, 1440#self.window.winfo_screenwidth(), self.window.winfo_screenheight()
+        self.screenDimension = self.window.winfo_screenwidth(), self.window.winfo_screenheight()
+        global ris
+        ris = self.screenDimension[0]
         self.window.config(cursor="none")
         self.pizze = self.pizzeCreator()
         self.aggiunte = self.aggiunteCreator()
-        self.menu = Elenco(self.window, self.pizze, self.aggiunte, [25, 25, self.screenDimension[0]-50, self.screenDimension[1]-100], fname)
+        self.menu = Elenco(self.window, self.pizze, self.aggiunte, [resolutionConverter(25), resolutionConverter(25), self.screenDimension[0]-resolutionConverter(50), self.screenDimension[1]-resolutionConverter(100)], fname)
 
         self.ShowAll()
         self.Update()
@@ -172,6 +174,10 @@ class Fullscreen:
     def Update(self):
         self.menu.updateScritte()
         self.window.after(2000, self.Update)
+
+def resolutionConverter(n):
+    #converts the ratio from 2560x1440 to ris
+    return int(n/(2560/ris))
 
 if __name__ == '__main__':
     app = Fullscreen()
