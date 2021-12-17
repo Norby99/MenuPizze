e94677f0eb9ba0a8fd9f7bd5ff70692c5a03e94e
Norby99 <72309039+Norby99@users.noreply.github.com>
2021-12-17 15:40:38 +0100

    first working setup

--- pizzaMenu2.py
+++ pizzaMenu2.py
@@ -1,19 +1,28 @@
 from pizzaMenu import PizzaMenu
 from libraries.PizzaHandler import Pizzas
-from libraries.utils import waitForConnection
+from libraries.utils import waitForConnection, waitForFilesUpdate
 from libraries.windowsSpecs import WindowSpecs
+from libraries.elencoGenerator import Elenco
 
 class PizzaMenu2(PizzaMenu):
 
     def __init__(self):
         data = self.loadSetupData()
-        self.p = Pizzas(data)
+        self.pizza = Pizzas(data)
         self.pizzaTypesRequered = ["Impasto Napoletano", "Pizze Dolci"] # the pizza types that have to be visualized
-
-        self.p.downloadAllFromCloud()
+        waitForFilesUpdate()    # files are downloaded from the pizzaMenu2, so this class waits for the update
+        self.pizza.loadPizzasFromJson()
         
         self.tkWindowSetup()
         self.windowSpecs = WindowSpecs(self.screenDimension)
+        colors = data["colors"] # colors are taken from the setup file
+
+        self.pizze = self.pizzeCreator(self.pizzaTypesRequered)
+        self.menu = Elenco(self.window, self.pizze, [self.windowSpecs.resolutionConverter(25), self.windowSpecs.resolutionConverter(25), self.screenDimension[0]-self.windowSpecs.resolutionConverter(25), self.screenDimension[1]-self.windowSpecs.resolutionConverter(25)], data, colors, self.screenDimension)
+
+        self.ShowAll()
+        self.Update()
+        self.window.mainloop()
 
 if __name__ == '__main__':
     waitForConnection()
