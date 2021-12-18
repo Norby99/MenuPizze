307fdad2d8d91cf41672084e464547f279ed212d
Norby99 <72309039+Norby99@users.noreply.github.com>
2021-12-18 16:57:32 +0100

    removed unused methods

--- libraries/verticalGrid.py
+++ libraries/verticalGrid.py
@@ -3,6 +3,7 @@ from libraries.database import database
 import tkinter as tk
 from libraries.windowSpecs import WindowSpecs
 from libraries.gridTitleCell import TitleCell
+from libraries.gridPizzaCell import PizzaCell
 from PIL import ImageTk,Image 
 import os
 
@@ -18,7 +19,6 @@ class VerticalGrid:
         self.margini = margini
         self.cell_dimension = [(self.margini[2]-self.margini[0])/self.maxColumns, (self.margini[3]-self.margini[1])/self.righe_max]
         self.cells = []
-        self.setupFonts()
 
         self.db = database("localhost", jsonData["dbUserName"], jsonData["dbPassword"], jsonData["dbData"])
 
@@ -37,12 +37,14 @@ class VerticalGrid:
                     y = 0
                     x += 1
 
+                cellPosition = [self.margini[0]+self.cell_dimension[0]*x, self.margini[1]+self.cell_dimension[1]*y]
                 if "nome" in pizza:
-                    self.cell_coordinates = [self.margini[0]+self.cell_dimension[0]*x, self.margini[1]+self.cell_dimension[1]*y, self.margini[0]+self.cell_dimension[0]*(x+1), self.margini[1]+self.cell_dimension[1]*(y+1)]
-                    self.scritte(self.cell_coordinates, pizza)
+                    tempCell = PizzaCell(self.canvas, pizza["nome"], self.colors["titolo"], pizza["prezzo"], self.colors["price"], pizza["ingredienti"], self.colors["generic_text"], cellPosition, self.cell_dimension)
+                    #self.cell_coordinates = [self.margini[0]+self.cell_dimension[0]*x, self.margini[1]+self.cell_dimension[1]*y, self.margini[0]+self.cell_dimension[0]*(x+1), self.margini[1]+self.cell_dimension[1]*(y+1)]
+                    #self.scritte(self.cell_coordinates, pizza)
                 else:
-                    tempCell = TitleCell(self.canvas, pizza["tipo"], self.colors["p_tipo"], [self.margini[0]+self.cell_dimension[0]*x, self.margini[1]+self.cell_dimension[1]*y], [self.cell_dimension[0], self.cell_dimension[1]])
-                    self.cells.append(tempCell)
+                    tempCell = TitleCell(self.canvas, pizza["tipo"], self.colors["p_tipo"], cellPosition, self.cell_dimension)
+                self.cells.append(tempCell)
                 y += 1
 
     def showAggiunte(self):
@@ -61,36 +63,18 @@ class VerticalGrid:
         canvas = self.canvas.create_text(coords[0], coords[3]-self.windowSpecs.resolutionConverter(5), anchor= tk.SW, fill=self.colors["generic_text"],font=font_aggiunte, text= self.testo_aggiunte, width = (coords[2]-coords[0]))
         self.ScritteAggiunte.update({'aggiunte': canvas})
 
-    def showAllergeni():
-        pass
-
     def scritte(self, coords, pizza):
 
-        if "nome" in pizza:
+        ### NOME ###
+        self.canvas.create_text(coords[0]+self.windowSpecs.resolutionConverter(5), coords[1]+self.windowSpecs.resolutionConverter(5), anchor= tk.NW, fill=self.colors["titolo"],font=self.font_nome, text= pizza["nome"])
+        self.canvas.create_image(10, coords[1]+self.windowSpecs.resolutionConverter(5)+5, anchor=tk.NW, image=self.alleggeni["glutine"])
 
-            if pizza["nome"] == "Estiva":
-                pizza["ingredienti"] += " tutto fuori cottura"
-            
-            ### NOME ###
-            self.canvas.create_text(coords[0]+self.windowSpecs.resolutionConverter(5), coords[1]+self.windowSpecs.resolutionConverter(5), anchor= tk.NW, fill=self.colors["titolo"],font=self.font_nome, text= pizza["nome"])
-            self.canvas.create_image(10, coords[1]+self.windowSpecs.resolutionConverter(5)+5, anchor=tk.NW, image=self.alleggeni["glutine"])
+        ### PREZZO ###
+        self.canvas.create_text(coords[2]-self.windowSpecs.resolutionConverter(10), coords[1]+self.windowSpecs.resolutionConverter(10), anchor= tk.NE, fill=self.colors["price"],font=self.font_prezzo, text= pizza["prezzo"])
 
-            ### PREZZO ###
-            self.canvas.create_text(coords[2]-self.windowSpecs.resolutionConverter(10), coords[1]+self.windowSpecs.resolutionConverter(10), anchor= tk.NE, fill=self.colors["price"],font=self.font_prezzo, text= pizza["prezzo"])
+        ### INGREDIENTI ###
+        self.ScritteIngredienti.append(self.canvas.create_text(coords[0]+self.windowSpecs.resolutionConverter(5), (coords[3]+coords[1])/2-self.windowSpecs.resolutionConverter(10), anchor= tk.NW, fill=self.colors["generic_text"],font=self.font_ingredienti, text= pizza["ingredienti"], width=(self.cell_dimension[0]-self.windowSpecs.resolutionConverter(10))))
 
-            ### INGREDIENTI ###
-            self.ScritteIngredienti.append(self.canvas.create_text(coords[0]+self.windowSpecs.resolutionConverter(5), (coords[3]+coords[1])/2-self.windowSpecs.resolutionConverter(10), anchor= tk.NW, fill=self.colors["generic_text"],font=self.font_ingredienti, text= pizza["ingredienti"], width=(self.cell_dimension[0]-self.windowSpecs.resolutionConverter(10))))
-        else:
-            ### TIPO ###
-            a = 3
-            self.canvas.create_line(coords[0], coords[1], coords[2]-self.windowSpecs.resolutionConverter(40), coords[1], fill="#ff8000", width="3")
-            self.canvas.create_line(coords[0], coords[3]-self.windowSpecs.resolutionConverter(15), coords[2]-self.windowSpecs.resolutionConverter(40), coords[3]-self.windowSpecs.resolutionConverter(15), fill="#ff8000", width="3")
-
-            self.canvas.create_text(coords[0]+self.windowSpecs.resolutionConverter(5)+a, coords[3]-self.windowSpecs.resolutionConverter(25)+a, anchor= tk.SW, fill="#BBBBBB", font=self.font_tipo, text= pizza["tipo"]) # shadow text
-            self.canvas.create_text(coords[0]+self.windowSpecs.resolutionConverter(5)+1, coords[3]-self.windowSpecs.resolutionConverter(25)+1, anchor= tk.SW, fill="#000000", font=self.font_tipo, text= pizza["tipo"]) # outer outline
-            self.canvas.create_text(coords[0]+self.windowSpecs.resolutionConverter(5)-1, coords[3]-self.windowSpecs.resolutionConverter(25)-1, anchor= tk.SW, fill="#000000", font=self.font_tipo, text= pizza["tipo"]) # inner outline
-
-            self.canvas.create_text(coords[0]+self.windowSpecs.resolutionConverter(5), coords[3]-self.windowSpecs.resolutionConverter(25), anchor= tk.SW, fill=self.colors["p_tipo"], font=self.font_tipo, text= pizza["tipo"]) # text body
 
     def updateScritte(self):    #this function updates the text boxes
         lingua = self.getLingua()
@@ -116,12 +100,6 @@ class VerticalGrid:
         glutine = ImageTk.PhotoImage(Image.open(os.path.join(targetFile, "glutine.png")).resize(resizeFormat, Image.ANTIALIAS))
         latticini = ImageTk.PhotoImage(Image.open(os.path.join(targetFile, "latticini.png")))
         return { "uova" : uova, "pesce" : pesce, "noci" : noci, "soia" : soia, "glutine" : glutine, "latticini" : latticini }
-
-    def setupFonts(self):
-        self.font_nome = "Times " + str(self.windowSpecs.resolutionConverter(20)) + " bold"
-        self.font_prezzo = "Times " + str(self.windowSpecs.resolutionConverter(18)) + " bold"
-        self.font_ingredienti = "Times " + str(self.windowSpecs.resolutionConverter(15))
-        self.font_tipo = "Times " + str(self.windowSpecs.resolutionConverter(36)) + " bold"
 
     def getLingua(self):
         return self.db.getCurrentLanguage()
