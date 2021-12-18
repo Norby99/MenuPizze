35fb0e672ff4d7778881d5c2afa185cbcf8f96b1
Norby99 <72309039+Norby99@users.noreply.github.com>
2021-12-18 15:50:57 +0100

    added methods to convert positions relative to the cell

--- libraries/gridCell.py
+++ libraries/gridCell.py
@@ -8,3 +8,17 @@ class Cell(ABC):
         self.dimensions = dimensions
         self.canvas = canvas
         self.windowSpecs = WindowSpecs()
+
+    def relativeXPostion(self, pos):
+        """Returns the relative X position of a given coordinate"""
+        if pos >= 0:
+            return self.position[0]+pos
+        else:
+            return self.position[0]+self.dimensions[0]+pos
+
+    def relativeYPostion(self, pos):
+        """Returns the relative Y position of a given coordinate"""
+        if pos >= 0:
+            return self.position[1]+pos
+        else:
+            return self.position[1]+self.dimensions[1]+pos
