011755ff536d4364b0382ac7aed4eab367044f9f
Norby99 <72309039+Norby99@users.noreply.github.com>
2021-12-18 15:25:47 +0100

    added method to set the title

--- libraries/gridTitleCell.py
+++ libraries/gridTitleCell.py
@@ -2,5 +2,10 @@ from libraries.gridCell import Cell
 
 class TitleCell(Cell):
 
-    def __init__(self, position, dimensions):
+    def __init__(self, position, dimensions, title):
         super().__init__(position, dimensions)
+        self.title = title
+        self.setTitle(self.title)
+
+    def setTitle(self, title):
+        self.title = title
