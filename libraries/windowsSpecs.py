d24af717e60018198492ee77e1c10cf59eccf287
Norby99 <72309039+Norby99@users.noreply.github.com>
2021-12-18 13:24:50 +0100

    fixed typo and increased the usability

--- libraries/windowsSpecs.py
+++ libraries/windowsSpecs.py
@@ -1,8 +1,21 @@
+import tkinter
+
 class WindowSpecs:
 
-    def __init__(self, screenDimension):
-        self.screenDimension = screenDimension
+    def __init__(self):
+        root = tkinter.Tk()
+        self.screenDimension = root.winfo_screenwidth(), root.winfo_screenheight()
+        root.quit()
+        root.destroy()
 
-    #converts the ratio from 2560x1440 to ris
+    def getScreenDimension(self):
+        """ Returns a tuple containing the resolution of the display """
+        return self.screenDimension
+
     def resolutionConverter(self, n):
+        """Converts the ratio from 2560x1440 to ris"""
         return int(n/(2560/self.screenDimension[0]))
+
+if __name__ == "__main__":
+    w = WindowSpecs()
+    print(w.getScreenDimension())
