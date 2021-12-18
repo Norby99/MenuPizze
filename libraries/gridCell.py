9156c4a318896f5988819753c1416781a49a8720
Norby99 <72309039+Norby99@users.noreply.github.com>
2021-12-18 15:29:15 +0100

    added canvas to cell

--- libraries/gridCell.py
+++ libraries/gridCell.py
@@ -2,6 +2,7 @@ from abc import ABC
 
 class Cell(ABC):
 
-    def __init__(self, position, dimensions):
+    def __init__(self, canvas, position, dimensions):
         self.position = position
         self.dimensions = dimensions
+        self.canvas = canvas
