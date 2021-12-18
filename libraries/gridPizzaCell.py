23bd8139b74b0b4d1f02da08ac93f0dfb6c9b0b5
Norby99 <72309039+Norby99@users.noreply.github.com>
2021-12-18 17:11:29 +0100

    first working object

--- libraries/gridPizzaCell.py
+++ libraries/gridPizzaCell.py
@@ -16,7 +16,7 @@ class PizzaCell(Cell):
         self.nameFont = "Times " + str(self.windowSpecs.resolutionConverter(20)) + " bold"
         self.price = price  # price setup
         self.priceColor = priceColor
-        self.pricFont = "Times " + str(self.windowSpecs.resolutionConverter(18)) + " bold"
+        self.priceFont = "Times " + str(self.windowSpecs.resolutionConverter(18)) + " bold"
         self.ingredients = ingredients  # ingredients setup
         self.ingredientsColor = ingredientsColor
         self.ingredientsFont = "Times " + str(self.windowSpecs.resolutionConverter(15))
@@ -26,19 +26,19 @@ class PizzaCell(Cell):
         self.createIngredients()
 
     def createName(self):
-        pass
+        self.canvas.create_text(self.relativeXPostion(self.windowSpecs.resolutionConverter(5)), self.relativeYPostion(self.windowSpecs.resolutionConverter(5)), anchor= tk.NW, fill=self.nameColor,font=self.nameFont, text=self.name)
 
     def createPrice(self):
-        pass
+        self.canvas.create_text(self.relativeXPostion(-self.windowSpecs.resolutionConverter(10)), self.relativeYPostion(self.windowSpecs.resolutionConverter(10)), anchor= tk.NE, fill=self.priceColor,font=self.priceFont, text=self.price)
 
     def createIngredients(self):
-        pass
+        self.canvas.create_text(self.relativeXPostion(self.windowSpecs.resolutionConverter(5)), (self.relativeYPostion(-1)+self.relativeYPostion(0))/2-self.windowSpecs.resolutionConverter(10), anchor= tk.NW, fill=self.ingredientsColor,font=self.ingredientsFont, text=self.ingredients, width=(self.dimensions[0]-self.windowSpecs.resolutionConverter(10)))
 
-    def setnameFont(self, font):
+    def setNameFont(self, font):
         self.nameFont = font
 
-    def setpricFont(self, font):
-        self.pricFont = font
+    def setPriceFont(self, font):
+        self.priceFont = font
 
-    def setingredientsFont(self, font):
+    def setIngredientsFont(self, font):
         self.ingredientsFont = font
