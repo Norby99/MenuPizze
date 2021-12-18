436b08b0f0ba8eff9fe55041c7b95fb39ce7fd28
Norby99 <72309039+Norby99@users.noreply.github.com>
2021-12-18 15:50:38 +0100

    added border

--- libraries/gridTitleCell.py
+++ libraries/gridTitleCell.py
@@ -2,10 +2,15 @@ from libraries.gridCell import Cell
 
 class TitleCell(Cell):
 
-    def __init__(self, position, dimensions, title):
-        super().__init__(position, dimensions)
+    def __init__(self, canvas, position, dimensions, title):
+        super().__init__(canvas, position, dimensions)
         self.title = title
         self.setTitle(self.title)
+        self.setBorders()
 
     def setTitle(self, title):
         self.title = title
+
+    def setBorders(self):
+        self.canvas.create_line(self.relativeXPostion(0), self.relativeYPostion(0), self.relativeXPostion(-1)-self.windowSpecs.resolutionConverter(40), self.relativeYPostion(0), fill="#ff8000", width="3")
+        self.canvas.create_line(self.relativeXPostion(0), self.relativeYPostion(-1)-self.windowSpecs.resolutionConverter(15), self.relativeXPostion(-1)-self.windowSpecs.resolutionConverter(40), self.relativeYPostion(-1)-self.windowSpecs.resolutionConverter(15), fill="#ff8000", width="3")
