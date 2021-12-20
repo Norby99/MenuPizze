2873b516afe76b063606f995fee96bf4d70624df
Norby99 <72309039+Norby99@users.noreply.github.com>
2021-12-20 13:23:11 +0100

    adjusted font and spaceing for ingredients

--- libraries/gridPizzaCell.py
+++ libraries/gridPizzaCell.py
@@ -19,7 +19,7 @@ class PizzaCell(Cell):
         self.priceFont = "Times " + str(self.windowSpecs.resolutionConverter(18)) + " bold"
         self.ingredients = ingredients  # ingredients setup
         self.ingredientsColor = ingredientsColor
-        self.ingredientsFont = "Times " + str(self.windowSpecs.resolutionConverter(15))
+        self.ingredientsFont = "Times " + str(self.windowSpecs.resolutionConverter(14))
 
         self.createName()
         self.createPrice()
@@ -32,7 +32,7 @@ class PizzaCell(Cell):
         self.textPrice = self.canvas.create_text(self.dimensions[0]-self.windowSpecs.resolutionConverter(10), self.windowSpecs.resolutionConverter(5), anchor= tk.NE, fill=self.priceColor,font=self.priceFont, text=self.price)
 
     def createIngredients(self):
-        self.textIngredients = self.canvas.create_text(self.windowSpecs.resolutionConverter(5), self.dimensions[1]/2-self.windowSpecs.resolutionConverter(20), anchor= tk.NW, fill=self.ingredientsColor,font=self.ingredientsFont, text=next(iter(self.ingredients)), width=(self.dimensions[0]-self.windowSpecs.resolutionConverter(5)))
+        self.textIngredients = self.canvas.create_text(self.windowSpecs.resolutionConverter(5), self.dimensions[1]/2-self.windowSpecs.resolutionConverter(13), anchor= tk.NW, fill=self.ingredientsColor,font=self.ingredientsFont, text=next(iter(self.ingredients)), width=(self.dimensions[0]-self.windowSpecs.resolutionConverter(5)))
 
     def setNameFont(self, font):
         self.nameFont = font
