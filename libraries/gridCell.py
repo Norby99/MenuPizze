633e4ed283e4221e48e5e9c37dc6eb47ac964adf
Norby99 <72309039+Norby99@users.noreply.github.com>
2021-12-20 14:37:11 +0100

    added postion getters and setters, automaticly generating hight

--- libraries/gridCell.py
+++ libraries/gridCell.py
@@ -4,11 +4,24 @@ from abc import ABC
 
 class Cell(ABC):
 
-    def __init__(self, window, position, dimensions):
-        """screenDim is an object that is used to avoid creating a tkinter class every time"""
+    def __init__(self, window, position, width, proportion=5.77841726618705):
+        """
+        window is the tkinter root object
+        given a width it calculates it's hight by the given proportion
+        """
         self.position = position
-        self.dimensions = dimensions
+        self.dimensions = [width, width/proportion]
         self.window = window
-        self.canvas = tk.Canvas(self.window, bg=window["background"], width=dimensions[0], height=dimensions[1], highlightthickness=0, bd=0)
+        self.canvas = tk.Canvas(self.window, bg=window["background"], width=self.dimensions[0], height=self.dimensions[1], highlightthickness=0, bd=0)
         self.canvas.place(x=position[0], y=position[1], anchor=tk.NW)
         self.windowSpecs = WindowSpecs([self.window.winfo_screenwidth(), self.window.winfo_screenheight()])   # to increase performance
+
+    def getBottomCoordinate(self):
+        return self.position[1]+self.dimensions[1]
+
+    def getRightCoordinate(self):
+        return self.position[0]+self.dimensions[0]
+
+    def setPostion(self, coord):
+        self.position = coord
+        self.canvas.place(x=self.position[0], y=self.position[1], anchor=tk.NW)
