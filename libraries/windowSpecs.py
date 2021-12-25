31d25d653e16d427a9415c142187531cf3769b5a
Norby99 <72309039+Norby99@users.noreply.github.com>
2021-12-25 15:00:04 +0100

    added argument to reduce screen proporion

--- libraries/windowSpecs.py
+++ libraries/windowSpecs.py
@@ -2,19 +2,27 @@ import tkinter
 
 class WindowSpecs:
 
-    def __init__(self, dimensions=[0,0]):
+    def __init__(self, dimensions=[0,0], checker=True):
+        """
+        Returns an object that handles the screen resolution
+        @dimensions if given avoids the tkinter windows creation, to encrease performance
+        @checker if enabled converts a doulbe monitor resolution to only one
+        """
         if dimensions == [0,0]:
             root = tkinter.Tk()
             self.screenDimension = [root.winfo_screenwidth(), root.winfo_screenheight()]
-
-            proportion = self.screenDimension[0]/self.screenDimension[1]
-            if not (1 < proportion < 2) :  # if the screen resolution isn't between 4:3 and 16:9 (for example a 32:9)
-                self.screenDimension[0] = int(self.screenDimension[0]/2)
-
             root.quit()
             root.destroy()
         else:
             self.screenDimension = dimensions
+
+        if checker:
+                self.convertDoubleMonitorResolution()
+
+    def convertDoubleMonitorResolution(self):
+        proportion = self.screenDimension[0]/self.screenDimension[1]
+        if not (1 < proportion < 2) :  # if the screen resolution isn't between 4:3 and 16:9 (for example a 32:9)
+            self.screenDimension[0] = int(self.screenDimension[0]/2)
 
     def getScreenDimension(self):
         """ Returns a tuple containing the resolution of the display """
