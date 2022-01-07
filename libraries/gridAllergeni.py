2c34c596982b833c75ca33e025f5b1408c5f15b2
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-01-07 21:36:16 +0100

    created first template

---
+++ libraries/gridAllergeni.py
@@ -0,0 +1,22 @@
+from libraries.gridCell import Cell
+import tkinter as tk
+import tkinter.font as TkFont
+
+class AllergeniCell(Cell):
+    """ This cell contains the legend of Allergens """
+
+    def __init__(self, window, allergens, textColor, position, width):
+        super().__init__(window, position, width, proportion=1)
+        self.allergens = allergens    # name setup
+        self.textColor = textColor
+        self.allergensFont = TkFont.Font(family="Times", size=self.windowSpecs.resolutionConverter(15), weight='bold')
+        self.canvas.configure(bg='cyan')    #! testing
+
+        self.createAllergens()
+
+    def createAllergens(self):
+        self.textNamePos = [self.windowSpecs.resolutionConverter(5), self.windowSpecs.resolutionConverter(5)]
+        self.textName = self.canvas.create_text(self.textNamePos[0], self.textNamePos[1], anchor= tk.NW, fill=self.nameColor,font=self.nameFont, text=self.name[next(iter(self.name))])
+
+    def setAllergensFontFont(self, font):
+        self.allergensFont = font
