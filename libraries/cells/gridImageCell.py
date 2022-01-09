8cd7c32aa793e71843b72a177cf22f8f0bba4efa
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-01-09 17:41:08 +0100

    corrected image position

--- libraries/cells/gridImageCell.py
+++ libraries/cells/gridImageCell.py
@@ -1,6 +1,6 @@
 from libraries.cells.gridCell import Cell
 import tkinter as tk
-from PIL import Image 
+import math
 
 class ImageCell(Cell):
     """
@@ -8,11 +8,11 @@ class ImageCell(Cell):
     """
     def __init__(self, window, image, position, width, proportion=1):
         super().__init__(window, position, width, proportion)
-        resizeFormat = (image.width(), image.height())
-        print(resizeFormat)
-        self.image = image._PhotoImage__photo.zoom(1)
+        resizeFormat = int(math.ceil(image.width()/width))
+        self.image = image._PhotoImage__photo.subsample(resizeFormat)
 
         self.showImage()
 
     def showImage(self):
-        self.canvas.create_image(0, 0, image=self.image, anchor=tk.NW)
+        center = (self.dimensions[0]/2, self.dimensions[1]/2)
+        self.canvas.create_image(center[0], center[1], image=self.image, anchor=tk.CENTER)
