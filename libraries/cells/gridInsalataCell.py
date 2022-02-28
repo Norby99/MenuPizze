ebc8682f3987d7fe6304bd974c0cdd0f2f44f2a0
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-02-28 23:53:59 +0100

    removed redundant rows

--- libraries/cells/gridInsalataCell.py
+++ libraries/cells/gridInsalataCell.py
@@ -12,15 +12,6 @@ class InsalataCell(PizzaCell):
     """
     def __init__(self, window, name, nameColor, price, priceColor, ingredients, ingredientsColor, allergens, position, width, proportion=5.76):
         super().__init__(window, name, nameColor, price, priceColor, ingredients, ingredientsColor, allergens, position, width, proportion=proportion)
-        self.name = name    # name setup
-        self.nameColor = nameColor
-        self.nameFont = TkFont.Font(family="Times", size=self.windowSpecs.resolutionConverter(20), weight='bold')
-        self.price = price  # price setup
-        self.priceColor = priceColor
-        self.priceFont = TkFont.Font(family="Times", size=self.windowSpecs.resolutionConverter(18), weight='bold')
-        self.ingredients = ingredients  # ingredients setup
-        self.ingredientsColor = ingredientsColor
-        self.ingredientsFont = TkFont.Font(family="Times", size=self.windowSpecs.resolutionConverter(15))
         self.allergens = allergens
         if self.allergens:
             self.allergensImageSize = self.allergens[0].width()
