da788d2b49cc96292e85f0f8c06b9b883356e820
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-02-28 23:28:04 +0100

    first working class

--- libraries/cells/gridSocialLogos.py
+++ libraries/cells/gridSocialLogos.py
@@ -1,21 +1,27 @@
 from libraries.cells.gridCell import Cell
 import tkinter as tk
 from PIL import ImageTk,Image 
+import math
 
 class SocialLogos(Cell):
     """
     This cell contains the logo of the Pizzeria
     @param logos is an array of PIL images
     """
-    def __init__(self, window, logos, position, width, proportion=1):
+    def __init__(self, window, logos, position, width, proportion=5):
         super().__init__(window, position, width, proportion)
-        #* image_size = 4/5    # represents the size of the image compared to the cell size 
-        new_image_width = math.trunc(width * image_size)
-        new_image_height = math.trunc(image.size[1] * new_image_width / image.size[0])
-        self.image = ImageTk.PhotoImage(image.resize((new_image_width,new_image_height), Image.ANTIALIAS))
+
+        self.social_logos = []
+        image_size = 4/5    # represents the size of the image compared to the cell size 
+        for image in logos:
+            new_image_height = math.trunc(width / proportion * image_size)
+            new_image_width = math.trunc(image.size[0] * new_image_height / image.size[1])
+
+            self.social_logos.append(ImageTk.PhotoImage(image.resize((new_image_width, new_image_height), Image.ANTIALIAS)))
 
         self.showImage()
 
     def showImage(self):
-        center = (self.dimensions[0]/2, self.dimensions[1]/2)
-        self.canvas.create_image(center[0], center[1], image=self.image, anchor=tk.CENTER)
+        for logo in self.social_logos:
+            center = (self.dimensions[0]/2, self.dimensions[1]/2)
+            self.canvas.create_image(center[0], center[1], image=logo, anchor=tk.CENTER)
