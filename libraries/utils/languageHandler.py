f8a2cb7426a9efd0a5702870bc24dc8ee3a2744a
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-03-03 14:47:36 +0100

    first working setup

--- libraries/utils/languageHandler.py
+++ libraries/utils/languageHandler.py
@@ -2,15 +2,17 @@ import requests
 import json
 
 class LanguageHandler():
-    connectionTracker = True #remouves spam comments
+    connectionTracker = True    # remouves spam comments
 
-    def __init__(self, website, token=""):
+    def __init__(self, website, default_language, token=""):
         """
         This class reads the language (a text shown on a website)
         @param website is the link to the website (eg. 192.168.1.1/request)
+        @param default_language is the default language that will be returned in case the website is not reachable
         @param token is optinal and is the autentication token to log in the website
         """
         self.website = website
+        self.default_language = default_language
         self.token = token
 
     def getCurrentLanguage(self):
@@ -19,34 +21,26 @@ class LanguageHandler():
                 req = requests.get(self.website, headers={'X-Master-Key': self.token})
             else:
                 req = requests.get(self.website)
-            return req
+            req = req.text
+            if (req == ""):
+                if self.connectionTracker:
+                    print("No response, maybe the token is wrong\n")
+                    self.connectionTracker = False
+                return self.default_language
+            else:
+                self.connectionTracker = True
+                return req
 
         except requests.exceptions.RequestException as err:
-            print(f"Host is not responding!\n{err}")
-            return False
-
-    def temp(self):
-        try:
-            if self.restaurant_name != "":
-                self.con = self.connect()
-                cursor = self.con.cursor(buffered=True)
-                query = f"""
-                    SELECT language_name
-                    FROM `restaurants`
-                    INNER JOIN languages ON restaurants.id_language = languages.id
-                    WHERE restaurants.restaurant_name = '{self.restaurant_name}';
-                    """
-                cursor.execute(query)
-                self.con.close()
-                return cursor.fetchone()[0]
-            else:
-                return "nome_italiano"
-        except:
-            return "nome_italiano"
+            if self.connectionTracker:
+                print(f"Host is not responding!\n{err}")
+                self.connectionTracker = False
+            return self.default_language
 
 if __name__ == "__main__":
     with open("DBsetup.json") as f:
         data = json.load(f)
-    LHandler = LanguageHandler(data['languageSite'] + "/" + data['restaurantName'] + ".php", token=data['m_key'])
+    LHandler = LanguageHandler(data['languageSite'] + "/" + data['restaurantName'] + ".php", data['defaultLanguage'], token=data['m_key'])
     while True:
         print(LHandler.getCurrentLanguage())
+
