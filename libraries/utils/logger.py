6766d8b5865b23323c6f921d25a250bc2ce2bb71
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-09-02 21:14:22 +0200

    added now line to write and removed comment

--- libraries/utils/logger.py
+++ libraries/utils/logger.py
@@ -8,12 +8,11 @@ class Logger():
         self._log_file = log_file
 
     def write(self, msg):
-        # throw an error
         if self._log_file is None:
             raise AttributeError("Log file not specified")
 
         with open(self._log_file, 'a') as f:
-            f.write(self._text_converter(msg))
+            f.write(f"{self._text_converter(msg)}\n")
 
     def disp(self, msg):
         print(self._text_converter(msg))
